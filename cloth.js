8ab4cde7b2d95ab143488427921589666de33aef
Ashley Kim <kitetale248@gmail.com>
2023-03-09 00:57:45 -0500

    created stretch, sheer strings in cloth

--- cloth.js
+++ cloth.js
@@ -1,3 +1,25 @@
+let maxWidth = 650;
+let maxHeight = 480;
+let padding = 50;
+
+function vec3plus (a,b){
+    return [a[0]+b[0],a[1]+b[1],a[2]+b[2]];
+}
+function vec3minus (a,b){
+    return [a[0]-b[0],a[1]-b[1],a[2]-b[2]];
+}
+function vec3length(a){
+    return Math.sqrt(a[0]*a[0]+a[1]*a[1]+a[2]*a[2]);
+}
+function vec3mult(a,s){
+    return [a[0]*s,a[1]*s,a[2]*s];
+}
+function vec3div(a,s){
+    return [a[0]/s,a[1]/s,a[2]/s];
+}
+function vec3dot(a,b){
+    return (a[0]*b[0]+a[1]*b[1]+a[2]*b[2]);
+}
 // --------------------------- PARTICLE -------------------------------
 class Particle {
     constructor(ConstructPos,ctx){
@@ -25,6 +47,11 @@ class Particle {
         this.ctx.fill();
     }
 
+    clearForce() {
+	    //reset applied force on this particle
+	    this.m_ForceAccumulator = [0,0,0];
+    }
+
 }
 
 
@@ -49,15 +76,15 @@ class SpringForce{
 
     apply_force(){
         // accumulated force of the first particle
-        let pdiff = this.m_p1.m_Position - this.m_p2.m_Position;
-        let vdiff = this.m_p1.m_Velocity - this.m_p2.m_Velocity;
-        var pdiffmag = length(pdiff);
-        let pdiffnorm = pdiff / pdiffmag;
+        let pdiff = vec3minus(this.m_p1.m_Position,this.m_p2.m_Position);
+        let vdiff = vec3minus(this.m_p1.m_Velocity,this.m_p2.m_Velocity);
+        var pdiffmag = vec3length(pdiff);
+        let pdiffnorm = vec3div(pdiff,pdiffmag);
 
         var spring = this.m_ks * (pdiffmag-this.m_dist);
-        var damping = this.m_kd * (vdiff*pdiff) / pdiffmag;
+        var damping = this.m_kd * vec3dot(vdiff,pdiff) / pdiffmag;
 
-        let force1 = -(spring+damping) * pdiffnorm;
+        let force1 = vec3mult(pdiffnorm,-(spring+damping));
 
         // set accumulated force of the first particle
         this.m_p1.m_ForceAccumulator = force1;
@@ -77,17 +104,54 @@ let fVector = [];
 const damp = 0.98;
 
 class Cloth{
-    constructor(ctx){
-        this.dt = 0.1;
+    constructor(rowN,colN,ctx){
+        this.dt = 0.01;
+        this.rowN = rowN;
+        this.colN = colN;
         this.ctx = ctx;
-        let dist = 2.0;
 
-        pVector.push(new Particle([50.0,100.0,1.0], ctx));
-        pVector.push(new Particle([150.0,100.0,1.0], ctx));
-        pVector.push(new Particle([250.0,100.0,1.0], ctx));
+        let xOffset = (maxWidth-padding*2)/(colN-1);
+        let yOffset = (maxHeight-padding*2)/(rowN-1);
+        let restLength = 100.0;
 
-        fVector.push(new SpringForce(pVector[0], pVector[1], dist, 1.0, 1.0, ctx));
-        fVector.push(new SpringForce(pVector[1], pVector[2], dist, 1.0, 1.0, ctx));
+        let ks_stretch = 0.1;
+        let kd_stretch = 0.1;
+        let ks_sheer = 0.1;
+        let kd_sheer = 0.1;
+        let ks_bend = 0.1;
+        let kd_bend = 0.1;
+
+        // particle construct
+        for (var i=0; i<rowN; i++){
+            for (var j=0; j<colN; j++){
+                pVector.push(new Particle([padding+j*xOffset,padding+i*yOffset,1.0],ctx))
+            }
+        }
+
+        // spring force construct
+        // stretch strings
+        for (var i=0; i<rowN; i++){
+            for (var j=0; j<colN; j++){
+                // horizontal
+                if (j<colN-1){
+                    fVector.push(new SpringForce(pVector[i*colN+j], pVector[i*colN+(j+1)], restLength, kd_stretch, ks_stretch, ctx));
+                }
+                // vertical
+                if (i<rowN-1){
+                    fVector.push(new SpringForce(pVector[i*colN+j], pVector[(i+1)*colN+j], restLength, kd_stretch, ks_stretch, ctx));
+                }
+            }
+        }
+        // sheer strings
+        for (var i=0; i<rowN-1; i++){
+            for (var j=0; j<colN-1; j++){
+                // \ diagonal
+                fVector.push(new SpringForce(pVector[i*colN+j], pVector[(i+1)*colN+(j+1)], restLength, kd_sheer, ks_sheer, ctx));
+                // / diagonal
+                fVector.push(new SpringForce(pVector[(i+1)*colN+j], pVector[i*colN+(j+1)], restLength, kd_sheer, ks_sheer, ctx));
+            }
+        }
+
     }
 
     reset(){
@@ -105,7 +169,23 @@ class Cloth{
     
         size = fVector.length;
         for(var ii=0; ii<size; ii++){
+            if (ii<(this.rowN*(this.colN-1)+this.colN*(this.rowN-1))){ 
+                ctx.strokeStyle = 'Crimson';
+            } else if (ii>=(this.rowN*(this.colN-1)+this.colN*(this.rowN-1))){ 
+                ctx.strokeStyle = 'ForestGreen';
+            } else {
+                ctx.strokeStyle = 'SteelBlue';
+            }
             fVector[ii].draw();
+        }
+    }
+
+    euler_step(){
+        let size = pVector.length;
+    
+        for(var ii=0; ii<size; ii++){
+            pVector[ii].m_Position = vec3plus(pVector[ii].m_Position, vec3mult(pVector[ii].m_Velocity, this.dt));
+            pVector[ii].m_Velocity = vec3plus(vec3mult(pVector[ii].m_Velocity, damp), vec3mult(pVector[ii].m_ForceAccumulator, this.dt/pVector[ii].m_Mass));
         }
     }
 
@@ -117,8 +197,9 @@ class Cloth{
         }
     
         ///second, apply forces to them
-        let fsize = fVector.length;
+        let fsize = pVector.length;
         for (var i=0; i<fsize; ++i){
+            // pVector[i].m_ForceAccumulator = [0,9.81,0];
             fVector[i].apply_force();
         }
     
@@ -127,27 +208,16 @@ class Cloth{
     
         ///Then, we can move forward
         ///Change this to others if you want to implement RK2 or RK4 or other integration method
-        euler_step();
+        this.euler_step();
     
         ///Finally, if you want to implement collisions, you could solve them here
         ///for the basic cloth simulation, you can skip this.
-    }
-
-    euler_step(){
-        let size = pVector.size();
-    
-        for(var ii=0; ii<size; ii++){
-            pVector[ii].m_Position += this.dt*pVector[ii].m_Velocity;
-            pVector[ii].m_Velocity = damp*(pVector[ii].m_Velocity) + this.dt*pVector[ii].m_ForceAccumulator/pVector[ii].m_Mass;
-        }
     }
 }
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-ctx.fillStyle = 'black';
-ctx.strokeStyle = 'black';
-ctx.strokeRect(0,0,canvas.width,canvas.height);
+canvas.style.border = "1px solid black";
 
 // var p1 = new Particle([100.0,100.0,1.0],ctx);
 // p1.draw();
@@ -158,5 +228,13 @@ ctx.strokeRect(0,0,canvas.width,canvas.height);
 // var s1 = new SpringForce(p1,p2,1,5,5,ctx);
 // s1.draw();
 
-var cloth = new Cloth(ctx);
-cloth.draw();
+var cloth = new Cloth(5,5,ctx);
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    cloth.simulation_step();
+    cloth.draw();
+    window.requestAnimationFrame(draw);
+}
+// cloth.draw();
+window.requestAnimationFrame(draw);
